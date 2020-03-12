7c5579c475b459305917df40a90ea52888520d26
miso-xyz <61099345+miso-xyz@users.noreply.github.com>
2020-03-13 00:11:54 +0100

    Add files via upload

---
+++ BrandIntegratorV2dotNet/BrandIntegratorV2dotNet/Form1.Designer.vb
@@ -0,0 +1,284 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim TreeNode160 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Blogspot.com")
+        Dim TreeNode161 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Wordpress.com")
+        Dim TreeNode162 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("BlogSky.com")
+        Dim TreeNode163 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Blogs.sapo.pt")
+        Dim TreeNode164 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Weebly.com")
+        Dim TreeNode165 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Altervista.org")
+        Dim TreeNode166 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Livejournal.com")
+        Dim TreeNode167 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Blogs", New System.Windows.Forms.TreeNode() {TreeNode160, TreeNode161, TreeNode162, TreeNode163, TreeNode164, TreeNode165, TreeNode166})
+        Dim TreeNode168 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Supersite.fr")
+        Dim TreeNode169 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("4yu.fr")
+        Dim TreeNode170 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Max.st")
+        Dim TreeNode171 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("3gp.fr")
+        Dim TreeNode172 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Gtx.fr")
+        Dim TreeNode173 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Ici.st")
+        Dim TreeNode174 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Site.cx")
+        Dim TreeNode175 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("User.fr")
+        Dim TreeNode176 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Zxr.fr")
+        Dim TreeNode177 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("R5t.com")
+        Dim TreeNode178 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Arab.ir")
+        Dim TreeNode179 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Denmark.ir")
+        Dim TreeNode180 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Egypt.ir")
+        Dim TreeNode181 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Icq.ir")
+        Dim TreeNode182 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Ir.ae")
+        Dim TreeNode183 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Ire.ir")
+        Dim TreeNode184 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Ireland.ir")
+        Dim TreeNode185 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Irq.ir")
+        Dim TreeNode186 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Irr.ir")
+        Dim TreeNode187 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Jpg.ir")
+        Dim TreeNode188 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Ksa.ir")
+        Dim TreeNode189 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Kuwait.ir")
+        Dim TreeNode190 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("London.ir")
+        Dim TreeNode191 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Paltalk.ir")
+        Dim TreeNode192 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Spain.ir")
+        Dim TreeNode193 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Sweden.ir")
+        Dim TreeNode194 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Tokyo.ir")
+        Dim TreeNode195 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Sx.de (Adult)")
+        Dim TreeNode196 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("MyRED Subdomains", New System.Windows.Forms.TreeNode() {TreeNode168, TreeNode169, TreeNode170, TreeNode171, TreeNode172, TreeNode173, TreeNode174, TreeNode175, TreeNode176, TreeNode177, TreeNode178, TreeNode179, TreeNode180, TreeNode181, TreeNode182, TreeNode183, TreeNode184, TreeNode185, TreeNode186, TreeNode187, TreeNode188, TreeNode189, TreeNode190, TreeNode191, TreeNode192, TreeNode193, TreeNode194, TreeNode195})
+        Dim TreeNode197 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Klinform.net")
+        Dim TreeNode198 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("000webhostapp.com")
+        Dim TreeNode199 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Wixsite.com")
+        Dim TreeNode200 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Tripod.com")
+        Dim TreeNode201 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Altervista.org")
+        Dim TreeNode202 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("HTML/PHP/Perl Hosting", New System.Windows.Forms.TreeNode() {TreeNode197, TreeNode198, TreeNode199, TreeNode200, TreeNode201})
+        Dim TreeNode203 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("shortlinknet.com")
+        Dim TreeNode204 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("payshorturl.com")
+        Dim TreeNode205 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("pinkhindi.com")
+        Dim TreeNode206 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("tmearn.com")
+        Dim TreeNode207 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("soft24.net")
+        Dim TreeNode208 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("URL Shorteners", New System.Windows.Forms.TreeNode() {TreeNode203, TreeNode204, TreeNode205, TreeNode206, TreeNode207})
+        Dim TreeNode209 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("informe.com")
+        Dim TreeNode210 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("3xforum.ro")
+        Dim TreeNode211 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("myforum.ro")
+        Dim TreeNode212 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Free Forum Hosting", New System.Windows.Forms.TreeNode() {TreeNode209, TreeNode210, TreeNode211})
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackgroundImage = Global.BrandIntegratorV2dotNet.My.Resources.Resources.download__1_
+        Me.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.TreeView1)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(325, 229)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(238, Byte), Integer), CType(CType(238, Byte), Integer), CType(CType(238, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(185, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(128, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Name of page (http(s)://):"
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Location = New System.Drawing.Point(12, 12)
+        Me.TreeView1.Name = "TreeView1"
+        TreeNode160.Name = "Node5"
+        TreeNode160.Text = "Blogspot.com"
+        TreeNode161.Name = "Node7"
+        TreeNode161.Text = "Wordpress.com"
+        TreeNode162.Name = "Node8"
+        TreeNode162.Text = "BlogSky.com"
+        TreeNode163.Name = "Node9"
+        TreeNode163.Text = "Blogs.sapo.pt"
+        TreeNode164.Name = "Node10"
+        TreeNode164.Text = "Weebly.com"
+        TreeNode165.Name = "Node11"
+        TreeNode165.Text = "Altervista.org"
+        TreeNode166.Name = "Node12"
+        TreeNode166.Text = "Livejournal.com"
+        TreeNode167.Name = "Node0"
+        TreeNode167.Text = "Blogs"
+        TreeNode168.Name = "Node13"
+        TreeNode168.Text = "Supersite.fr"
+        TreeNode169.Name = "Node14"
+        TreeNode169.Text = "4yu.fr"
+        TreeNode170.Name = "Node15"
+        TreeNode170.Text = "Max.st"
+        TreeNode171.Name = "Node16"
+        TreeNode171.Text = "3gp.fr"
+        TreeNode172.Name = "Node17"
+        TreeNode172.Text = "Gtx.fr"
+        TreeNode173.Name = "Node18"
+        TreeNode173.Text = "Ici.st"
+        TreeNode174.Name = "Node19"
+        TreeNode174.Text = "Site.cx"
+        TreeNode175.Name = "Node20"
+        TreeNode175.Text = "User.fr"
+        TreeNode176.Name = "Node21"
+        TreeNode176.Text = "Zxr.fr"
+        TreeNode177.Name = "Node24"
+        TreeNode177.Text = "R5t.com"
+        TreeNode178.Name = "Node25"
+        TreeNode178.Text = "Arab.ir"
+        TreeNode179.Name = "Node26"
+        TreeNode179.Text = "Denmark.ir"
+        TreeNode180.Name = "Node27"
+        TreeNode180.Text = "Egypt.ir"
+        TreeNode181.Name = "Node28"
+        TreeNode181.Text = "Icq.ir"
+        TreeNode182.Name = "Node29"
+        TreeNode182.Text = "Ir.ae"
+        TreeNode183.Name = "Node30"
+        TreeNode183.Text = "Ire.ir"
+        TreeNode184.Name = "Node31"
+        TreeNode184.Text = "Ireland.ir"
+        TreeNode185.Name = "Node32"
+        TreeNode185.Text = "Irq.ir"
+        TreeNode186.Name = "Node33"
+        TreeNode186.Text = "Irr.ir"
+        TreeNode187.Name = "Node34"
+        TreeNode187.Text = "Jpg.ir"
+        TreeNode188.Name = "Node35"
+        TreeNode188.Text = "Ksa.ir"
+        TreeNode189.Name = "Node36"
+        TreeNode189.Text = "Kuwait.ir"
+        TreeNode190.Name = "Node37"
+        TreeNode190.Text = "London.ir"
+        TreeNode191.Name = "Node38"
+        TreeNode191.Text = "Paltalk.ir"
+        TreeNode192.Name = "Node39"
+        TreeNode192.Text = "Spain.ir"
+        TreeNode193.Name = "Node40"
+        TreeNode193.Text = "Sweden.ir"
+        TreeNode194.Name = "Node23"
+        TreeNode194.Text = "Tokyo.ir"
+        TreeNode195.Name = "Node22"
+        TreeNode195.Text = "Sx.de (Adult)"
+        TreeNode196.Name = "Node1"
+        TreeNode196.Text = "MyRED Subdomains"
+        TreeNode197.Name = "Node41"
+        TreeNode197.Text = "Klinform.net"
+        TreeNode198.Name = "Node42"
+        TreeNode198.Text = "000webhostapp.com"
+        TreeNode199.Name = "Node43"
+        TreeNode199.Text = "Wixsite.com"
+        TreeNode200.Name = "Node44"
+        TreeNode200.Text = "Tripod.com"
+        TreeNode201.Name = "Node45"
+        TreeNode201.Text = "Altervista.org"
+        TreeNode202.Name = "Node2"
+        TreeNode202.Text = "HTML/PHP/Perl Hosting"
+        TreeNode203.Name = "Node46"
+        TreeNode203.Text = "shortlinknet.com"
+        TreeNode204.Name = "Node47"
+        TreeNode204.Text = "payshorturl.com"
+        TreeNode205.Name = "Node48"
+        TreeNode205.Text = "pinkhindi.com"
+        TreeNode206.Name = "Node49"
+        TreeNode206.Text = "tmearn.com"
+        TreeNode207.Name = "Node50"
+        TreeNode207.Text = "soft24.net"
+        TreeNode208.Name = "Node3"
+        TreeNode208.Text = "URL Shorteners"
+        TreeNode209.Name = "Node51"
+        TreeNode209.Text = "informe.com"
+        TreeNode210.Name = "Node52"
+        TreeNode210.Text = "3xforum.ro"
+        TreeNode211.Name = "Node53"
+        TreeNode211.Text = "myforum.ro"
+        TreeNode212.Name = "Node4"
+        TreeNode212.Text = "Free Forum Hosting"
+        Me.TreeView1.Nodes.AddRange(New System.Windows.Forms.TreeNode() {TreeNode167, TreeNode196, TreeNode202, TreeNode208, TreeNode212})
+        Me.TreeView1.Size = New System.Drawing.Size(167, 208)
+        Me.TreeView1.TabIndex = 4
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(185, 28)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(128, 20)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(185, 197)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(130, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Open in browser"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(185, 54)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(128, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "App info"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(325, 229)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Brand Integrator V2 (Remake)"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TreeView1 As System.Windows.Forms.TreeView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ BrandIntegratorV2dotNet/BrandIntegratorV2dotNet/Form1.vb
@@ -0,0 +1,74 @@
+﻿Public Class Form1
+    Dim web_name_s As Boolean
+    Dim web_host_s As Boolean
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'MsgBox(TreeView1.SelectedNode.Text)
+        Dim website_ As String = TreeView1.SelectedNode.Text
+        Select Case website_
+            Case "Blogspot.com"
+                website_ = "https://www.blogger.com/create-blog.g?defaultSubdomain=" & TextBox1.Text
+            Case "Wordpress.com"
+                website_ = "https://wordpress.com/start?aff=13056&new=" & TextBox1.Text
+            Case "BlogSky.com"
+                website_ = "http://www.blogsky.com/signup"
+            Case "Blogs.sapo.pt"
+                website_ = "https://blogs.sapo.pt/create"
+            Case "Weebly.com"
+                website_ = "https://www.weebly.com/features/start-a-blog?lang=en"
+            Case "Altervista.org"
+                website_ = "https://en.altervista.org/registration.php?action=form&atype=blog"
+            Case "Livejournal.com"
+                website_ = "https://www.livejournal.com/create.bml/?user=" & TextBox1.Text
+            Case "Supersite.fr", "4yu.fr"
+                website_ = "http://www.supersite.fr/myred/register.php?do=register2&domainname=" & TextBox1.Text & "&ext=" & website_
+            Case "Max.st", "3gp.fr", "Gtx.fr", "Ici.st", "Site.cx", "User.fr", "Zxr.fr"
+                website_ = "http://www.max.st/myred/register.php?do=register2&domainname=" & TextBox1.Text & "&ext=" & website_
+            Case "R5t.com"
+                website_ = "http://r5t.com/register.php?do=register2&domainname=" & TextBox1.Text & "&ext=r5t.com"
+            Case "Arab.ir", "Denmark.ir", "Egypt.ir", "Icq.ir", "Ir.ae", "Ire.ir", "Ireland.ir", "Irq.ir", "Irr.ir", "Jpg.ir", "Ksa.ir", "Kuwait.ir", "London.ir", "Paltalk.ir", "Spain.ir", "Sweden.ir", "Tokyo.ir"
+                website_ = "http://ir.ae/myred/register.php?do=register2&domainname=" & TextBox1.Text & "&ext=" & website_
+            Case "Sx.de (Adult)"
+                website_ = "http://www.sx.de/gratiserotikdomains/register.php?do=register2&domainname=" & TextBox1.Text & "&ext=sx.de"
+            Case Else
+                MessageBox.Show("Node is not valid!", "Brand Integrator V2", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                website_ = Nothing
+                TreeView1.SelectedNode = Nothing
+        End Select
+        Process.Start(website_)
+    End Sub
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        If TextBox1.Text = "" Then
+            web_name_s = False
+        Else
+            web_name_s = True
+        End If
+    End Sub
+
+    Private Sub TreeView1_AfterSelect(ByVal sender As System.Object, ByVal e As System.Windows.Forms.TreeViewEventArgs) Handles TreeView1.AfterSelect
+        If TreeView1.SelectedNode.Text = "" Then
+            web_host_s = False
+        Else
+            web_host_s = True
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If web_host_s = True And web_name_s = True Then
+            Button1.Enabled = True
+        Else
+            Button1.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Timer1.Start()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If MessageBox.Show("App made by miso" & vbCrLf & "Original post by hacxx" & vbCrLf & "Released on sinister.ly (<3)" & vbCrLf & vbCrLf & "See hacxx's original post?", "Brand Integrator V2", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+            Process.Start("https://sinister.ly/Thread-Brand-Integrator-v2-4-Register-and-spread-your-brand-for-free")
+        Else
+            MsgBox(":c", MsgBoxStyle.OkOnly, "Brand Integrator V2")
+        End If
+    End Sub
+End Class
